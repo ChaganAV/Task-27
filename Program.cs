a267b82d35d86b18530e1cae3a609631fa0276f3
Alexandr <jwchaganav@gmail.com>
2022-11-08 14:35:57 +0400

    create

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int number = 0;
+int summaNumbers = 0;
+
+Console.WriteLine("Введите число");
+if(Int32.TryParse(Console.ReadLine(),out number))
+{
+    summaNumbers = SumNumbers(number);
+    Console.WriteLine($"Сумма цифр в указанном числе: {summaNumbers}");
+}
+else
+{
+    Console.WriteLine("Необходимо ввести число!");
+    return;
+}
+
+
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    while(num>0)
+    {
+        sum+=num%10;
+        num = num/10;
+    }
+    return sum;
+}
